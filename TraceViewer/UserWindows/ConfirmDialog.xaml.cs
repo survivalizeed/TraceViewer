fb6995c7b2d168ac3fb4348e91bdec2e94e765aa
survivalizeed <gerbdm@gmail.com>
2025-04-02 16:04:26 +0200

    added a confirm dialog

---
+++ TraceViewer/UserWindows/ConfirmDialog.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TraceViewer.UserControls
+{
+
+
+    public partial class ConfirmDialog : Window
+    {
+        private bool result = false;
+
+        public ConfirmDialog(string Prompt)
+        {
+            InitializeComponent();
+            this.PromptText.Text = Prompt;
+            this.Owner = Application.Current.MainWindow;
+            this.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+        }
+
+        private void Ok_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                result = true;
+                this.Close();
+            }
+        }
+
+        private void Cancel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.Close();
+        }
+
+        public bool GetResult()
+        {
+            return result;
+        }
+    }
+}
